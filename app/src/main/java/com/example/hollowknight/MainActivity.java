77a86d8cfa6e5d1e405e6758e01aa5917e0481c6
Administrador <felipe.oropeza@etec.sp.gov.br>
2023-06-16 08:39:46 -0300

    Começando botão

--- app/src/main/java/com/example/hollowknight/MainActivity.java
+++ app/src/main/java/com/example/hollowknight/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.hollowknight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnentrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnentrar = (Button) findViewById (R.id.btnentrar);
+        btnentrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent menu = new Intent(getApplicationContext(), Menu.class);
+                startActivity(menu);
+            }
+        });
     }
+
+
 }
